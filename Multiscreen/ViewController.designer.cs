f814a8d521a7151472c655915a0877c99c52e912
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-12-18 23:48:27 -0600

    Inserción inicial en el repositorio del módulo Multiscreen

---
+++ Multiscreen/ViewController.designer.cs
@@ -0,0 +1,59 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Multiscreen
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CallButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CallHistoryButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PhoneNumberText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton TranslateButton { get; set; }
+
+        [Action ("TranslateButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void TranslateButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (CallButton != null) {
+                CallButton.Dispose ();
+                CallButton = null;
+            }
+
+            if (CallHistoryButton != null) {
+                CallHistoryButton.Dispose ();
+                CallHistoryButton = null;
+            }
+
+            if (PhoneNumberText != null) {
+                PhoneNumberText.Dispose ();
+                PhoneNumberText = null;
+            }
+
+            if (TranslateButton != null) {
+                TranslateButton.Dispose ();
+                TranslateButton = null;
+            }
+        }
+    }
+}
